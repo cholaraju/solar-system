84347efd05df6650eea325a3961eb1d081546e94
cholaraju <rajuneluri@gmail.com>
2025-07-21 15:40:21 +0530

    changes

--- app.js
+++ app.js
@@ -35,26 +35,26 @@ var planetModel = mongoose.model('planets', dataSchema);
 
 
 
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+app.post('/planet', async function (req, res) {
+    try {
+        const planetData = await planetModel.findOne({ id: req.body.id });
+        if (!planetData) {
+            res.status(404).send("Planet not found");
         } else {
             res.send(planetData);
         }
-    })
-})
+    } catch (err) {
+        res.status(500).send("Error in Planet Data");
+    }
+});
+
 
 app.get('/',   async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 
-app.get('/os',   function(req, res) {
+app.get('/os',   async function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
@@ -62,14 +62,14 @@ app.get('/os',   function(req, res) {
     });
 })
 
-app.get('/live',   function(req, res) {
+app.get('/live',  async function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
 })
 
-app.get('/ready',   function(req, res) {
+app.get('/ready',   async function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
