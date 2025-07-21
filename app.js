d3bf87b861c0ddd3c74dc4ad9527581ac42a92c6
cholaraju <rajuneluri@gmail.com>
2025-07-21 15:23:53 +0530

    refactor app.js to improve code structure and readability

--- app.js
+++ app.js
@@ -12,17 +12,14 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
-})
+  if (err) {
+    console.log("error!! " + err);
+  }
+});
+
 
 var Schema = mongoose.Schema;
 
